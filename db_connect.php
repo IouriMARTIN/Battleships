d23ea0d746c16328cec325c0f10672a3f835affc
IouriMARTIN <iourimartin@gmail.com>
2024-12-13 09:23:06 +0100

    Battle

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,24 @@
 <?php
+  $host = 'localhost';
+  $dbname = 'BattleShips';
+  $username = 'ProjectSQL';
+  $password = 'Ships';
   
+  try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    $id = ;
+    $row = ;
+    $col = ;
+    $bateau_id = ;
+
+    $sql = "INSERT INTO ShipsPos () VALUES ()";
+    $stmt = = $pdo->prepare($sql);
+
+    $stmt->bindParam(':exemple', $exemple, PDO::PARAM_INT);
+
+    $stmt->execute();
+  } catch (PDOException $e) {
+    echo "Erreur de connexion ou d'exe de la requete :" . $e->message();
+  }
 ?>
